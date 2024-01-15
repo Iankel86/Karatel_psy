e18d5b2311ffb1e78d45c0e15f738a7fe631a30d
Iankel86 <yankelll86@gmail.com>
2024-01-16 00:59:09 +0300

    feat: site test No 1

--- quasar.config.js
+++ quasar.config.js
@@ -51,7 +51,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
-      publicPath: 'projectname',
+      // publicPath: 'projectname',
       target: {
         browser: ["es2019", "edge88", "firefox78", "chrome87", "safari13.1"],
         node: "node16",
